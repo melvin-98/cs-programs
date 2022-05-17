0ab98b2d024b44214f11c10499efd13af9c20b6e
Melvin Sunny <49115745+melvin-98@users.noreply.github.com>
2022-05-17 13:33:10 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace firstprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a_1 = 10.01;
+            int b = 10;
+            double aDouble = 52.0 / b;
+            uint v = -10;
+            WriteLine("Hello"+" "+a_1);
+            //WriteLine("Hello" + " " + b);
+            WriteLine("Hello "+ aDouble);
+            ReadKey();
+        }
+    }
+}
